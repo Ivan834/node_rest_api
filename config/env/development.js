ca4b539777574b1416aee124e061ac146e500be9
Ivan834 <pavlov.ivan0415@outlook.com>
2015-09-15 19:44:06 +0400

    commit4

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,6 @@
 module.exports = {
-	db: 'mongodb://localhost/test',
+	//db: 'mongodb://localhost/test',
+	db: 'mongodb://mongouser:mongo0915@ds027719.mongolab.com:27719/mongotest', // Mongolab
 	// Development configuration options
 	sessionSecret:'developmentSessionSecret'
 };
